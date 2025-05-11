571edbab19c879a36633fd064df6e4f708c898d1
Joseph Haddad <117955484+JFHaddad1@users.noreply.github.com>
2025-05-11 12:06:08 -0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   return {
     dir: {
       input: "src",
+      assets: "_assets",
       includes: "_includes",
       layouts: "_layouts",
       output: "docs"
